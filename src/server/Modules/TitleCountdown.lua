21edd34aceaac7dadd206b0a383f229a3792c5ad
Grant <javaughngrant28@gmail.com>
2025-03-06 09:28:03 -0500

    Timer Count down

---
+++ src/server/Modules/TitleCountdown.lua
@@ -0,0 +1,62 @@
+
+local MaidModule = require(game.ReplicatedStorage.Shared.Modules.Maid)
+local ScreenGuiUtil = require(game.ReplicatedStorage.Shared.Utils.ScreenGuiUtil)
+
+local DefaultGUI = game.ReplicatedStorage.Assets.ScreenGui.TitleCountdown
+
+local TitleCountdown = {}
+
+TitleCountdown.__TITLE = nil
+TitleCountdown.__DURATION = nil
+TitleCountdown.__GUI_REFERENCE = nil
+TitleCountdown._MAID = nil
+
+function TitleCountdown.new(title: string, duration: number, gui: ScreenGui?)
+
+    local self = setmetatable({}, { __index = TitleCountdown })
+
+    self.__TITLE = title
+    self.__DURATION = duration
+    self.__GUI_REFERENCE = gui or DefaultGUI
+    self._MAID = MaidModule.new()
+
+    self:_Start()
+
+    return self
+end
+
+function TitleCountdown:_Start()
+    local screenGui: ScreenGui = self.__GUI_REFERENCE
+    assert(screenGui and screenGui:IsA("ScreenGui"), `Invalid ScreenGui provided: {screenGui}`)
+
+    local guiClone = screenGui:Clone()
+    self._MAID['GUI CLONE'] = guiClone
+
+    if self.__TITLE then
+        local title = Instance.new('StringValue')
+        title.Name = 'TitleValue'
+        title.Value = self.__TITLE
+        title.Parent = guiClone
+    end
+
+    local countDownGoal = Instance.new('StringValue')
+    countDownGoal.Name = 'CountDownGoal'
+    countDownGoal.Value = tostring(os.time() + self.__DURATION)
+    countDownGoal.Parent = guiClone
+
+    self._MAID['Connection'] = ScreenGuiUtil.AddToAllPlayers(guiClone,true)
+
+    task.delay(self.__DURATION, function()
+        self:Destroy()
+    end)
+end
+
+function TitleCountdown:Destroy()
+    if not self or not self._MAID then return end
+    self._MAID:DoCleaning()
+    for key, _ in pairs(self) do
+        self[key] = nil
+    end
+end
+
+return TitleCountdown
